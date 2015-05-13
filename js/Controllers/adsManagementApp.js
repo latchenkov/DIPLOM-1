76e22a66cf663d48b95dac93d4ee5e1f9616a86a
Sergey Latchenkov <latchenkov@gmail.com>
2015-05-13 16:27:17 +0300

    vol. 1 v.7

--- js/Controllers/adsManagementApp.js
+++
@@ -1,8 +0,0 @@
-var adsManagementApp = angular.module('adsManagementApp', [])
-
-.constant('defaultValue', {
-default_ad : {type:'private',location_id:'Новосибирск', price:0.00, allow_mails:false},
-button_value : 'Добавить объявление'
-});
-
-
